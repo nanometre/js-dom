7500bec153484dc69b4ab613fd56c30f3ead088b
nanometre <benedictwcy@hotmail.com>
2022-02-08 04:02:42 +0000

    Add 03-modify-many folder

---
+++ 03-modify-many/script.js
@@ -0,0 +1,11 @@
+// r is an array
+let r = document.querySelectorAll('.recommended');
+for (let eachLi of r) {
+    eachLi.style.backgroundColor = 'green';
+}
+/*
+for (let i =0; i < r.length; i++) {
+    r[i].style.backgroundColor = 'green'
+}
+
+*/
