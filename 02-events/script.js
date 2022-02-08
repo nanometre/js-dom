9b3eb9389e35bcf199e95acf0bb4d1fb058404d1
nanometre <benedictwcy@hotmail.com>
2022-02-08 03:54:59 +0000

    Added more files to 02-events

--- 02-events/script.js
+++ 02-events/script.js
@@ -1,6 +1,11 @@
 let box = document.querySelector('#box')
+let count = 0
+
 box.addEventListener('mouseenter', function(){
     box.style.backgroundColor = 'blue'
+    count += 1
+    // box.innerHTML = count
+    document.querySelector('#output').innerHTML = count
 });
 box.addEventListener('mouseleave', function(){
     box.style.backgroundColor = 'white'
